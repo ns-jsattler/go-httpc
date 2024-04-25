65fdfda05de37ac72d0ead9491e74816cea89280
Jason Hancock <jason@jasonhancock.com>
2024-04-25 15:51:23 -0700

    fix a rare panic

--- errors/client.go
+++ errors/client.go
@@ -52,7 +52,7 @@ func NewClientErr(op string, err error, resp *http.Response, opts ...ClientOptFn
 		newClientErr.u = *req.URL
 		newClientErr.method = req.Method
 
-		if req.Header != nil && strings.Contains(req.Header.Get("Content-Type"), "application/json") {
+		if req.Header != nil && strings.Contains(req.Header.Get("Content-Type"), "application/json") && req.Body != nil {
 			if body, err := ioutil.ReadAll(req.Body); err == nil {
 				newClientErr.respBody = string(body)
 			}
